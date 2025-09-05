a0ab7f61eb3f5b2b6b4238007787b0679b20c383
Víctor Zambrano <donvito4571@gmail.com>
2025-09-05 18:33:46 -0400

    Mostrar bolívares

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,46 @@
 # frozen_string_literal: true
 
+require "json"
+require "net/http"
+
 class Main
+  attr_reader :errors, :dollars
+
+  def initialize(arguments)
+    @arguments = arguments
+    validate_arguments
+  end
+
   def call
-    puts message
+    if errors.empty?
+      printf("Son %.2f bolívares\n", price * dollars)
+      # puts "Son #{price * dollars} bolívares"
+    else
+      errors.each do |error|
+        puts error
+      end
+    end
+  end
+
+  def price
+    uri = URI("https://bcv-api.rafnixg.dev/rates/")
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)["dollar"]
+  end
+
+  private
+
+  def validate_arguments
+    @errors = []
+    if @arguments.length == 1
+      begin
+        @dollars = Float(@arguments.first)
+      rescue ArgumentError
+        @errors.push("El argumento debe ser un número")
+      end
+    else
+      @errors.push("Se requiere 1 argumento y tienes #{@arguments.length}")
+    end
   end
 
   def message
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,9 +3,17 @@
 require "main"
 
 RSpec.describe Main do
-  describe "#message" do
-    it "retrieves the message" do
-      expect(Main.new.message).to eq("Hola Victor")
+  describe "#price" do
+    it "Validates number of arguments" do
+      wrong_argument = Main.new([])
+      expect(wrong_argument.errors.length).to eq(1)
+      expect(wrong_argument.errors.first).to eq("Se requiere 1 argumento y tienes 0")
+    end
+
+    it "Validates input type" do
+      wrong_argument = Main.new(["some string"])
+      expect(wrong_argument.errors.length).to eq(1)
+      expect(wrong_argument.errors.first).to eq("El argumento debe ser un número")
     end
   end
 end
